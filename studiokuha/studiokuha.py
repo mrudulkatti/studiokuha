adbd2a183d858be47f5af233ed733c27fdd3c7aa
varunbheemaiah <varunbheemaiah9@gmail.com>
2020-02-11 09:26:08 +0530

    image centered

--- studiokuha/studiokuha.py
+++ studiokuha/studiokuha.py
@@ -1,14 +1,14 @@
-from flask import Flask , request , render_template, redirect , url_for, session, flash
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("first.html")
+    return render_template("index.html")
 
 @app.route("/home")
 def new():
     return render_template("studiokuha.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=80, threaded=True)
