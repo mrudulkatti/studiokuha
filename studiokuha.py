b260bc235762069167138d449feed3b29b8b4eef
varunbheemaiah <varunbheemaiah9@gmail.com>
2020-03-22 21:07:23 +0530

    services

--- studiokuha.py
+++ studiokuha.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
 @app.route("/home") #coming soon page
 def new():
     return render_template("studiokuha.html")
